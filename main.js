51dd5d75a174f2dd1de48f15911475da90b7c3a0
Keith Bloom <keith.bloom@gmail.com>
2020-06-26 12:17:21 +0100

    crash reporting

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+require('electron-local-crash-reporter').start();
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
     }
   })
 
@@ -16,7 +19,7 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
